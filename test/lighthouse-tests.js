57a3d2a039f33a62b5082357f399514562fe0837
alejandro <asanchezgiraldo@bauer-media.com.au>
2017-06-29 10:32:55 +1000

    adding retry for lighthouse test

--- test/lighthouse-tests.js
+++ test/lighthouse-tests.js
@@ -34,22 +34,24 @@ const auditConfig = require('./audits.json');
 // https://github.com/paulirish/pwmetrics/
 const ourMetrics = require('./metrics');
 
-describe('Lighthouse PWA Testing', function() {
+    describe('Lighthouse PWA Testing', function() {
+        // Retry all tests in this suite up to 2 times
+        this.retries(2);
 
-    // Failsafe; could be long depending on what you're trying to test
-    this.timeout(40000);
+        // Failsafe; could be long depending on what you're trying to test
+        this.timeout(40000);
 
-    // We'll run our lighthouse set once and store for compare in this sample
-    // you could very easily build a different sort of runner
-    let _lhResult = null;
+        // We'll run our lighthouse set once and store for compare in this sample
+        // you could very easily build a different sort of runner
+        let _lhResult = null;
 
-    before('Run Lighthouse base test', (done) => {
-        lighthouse(testUrl, lighthouseOptions, auditConfig)
-            .then((res) => {
-            _lhResult = ourMetrics.prepareData(res);
-    done();
-});
-});
+        beforeEach('Run Lighthouse base test', (done) => {
+            lighthouse(testUrl, lighthouseOptions, auditConfig)
+                .then((res) => {
+                _lhResult = ourMetrics.prepareData(res);
+        done();
+        });
+    });
 
 
 // We currenlty do not use Service Workers for Offline usage
@@ -62,25 +64,25 @@ describe('Lighthouse PWA Testing', function() {
 //});
 
 
-// Currently 1000ms is to high the example had 500ms
+    // Currently 1000ms is to high the example had 500ms
 
-it("should have first meaningful paint < 1000ms", (done) => {
-    let ttfmp = _lhResult.preparedResults.find(r => {
-            return r.name === 'ttfmp';
-});
-console.log("current reading is => " + ttfmp.value + "ms");
-assert.isBelow(ttfmp.value, 1000);
-done();
-});
+    it("should have first meaningful paint < 1000ms", (done) => {
+        let ttfmp = _lhResult.preparedResults.find(r => {
+                return r.name === 'ttfmp';
+        });
+        console.log("current reading is => " + ttfmp.value + "ms");
+        assert.isBelow(ttfmp.value, 1000);
+        done();
+    });
 
-// Currently 4000ms is to high the example had 1000ms
+    // Currently 4000ms is to high the example had 1000ms
 
-it("should have time to interactive < 4000ms", (done) => {
-    let tti = _lhResult.preparedResults.find(r => {
-            return r.name === 'tti';
-});
-console.log("current reading is => " + tti.value + "ms");
-assert.isBelow(tti.value, 4000);
-done();
-});
+    it("should have time to interactive < 4000ms", (done) => {
+        let tti = _lhResult.preparedResults.find(r => {
+                return r.name === 'tti';
+        });
+        console.log("current reading is => " + tti.value + "ms");
+        assert.isBelow(tti.value, 4000);
+        done();
+    });
 });
